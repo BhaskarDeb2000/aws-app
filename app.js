b865e860762048c6dd3c41bf0dbde642d80a3dea
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2025-03-07 13:46:07 +0200

    feat: deploy app to Elastic Beanstalk

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 
 // Define a route to handle GET requests
 app.get("/", (req, res) => {
-  res.send("Hello Elastic Beanstalk");
+  res.send(
+    "Hello, Elastic Beanstalk! The assignment has been submitted by Bhaskar Deb, Sabina Akter, and Saimun Islam"
+  );
 });
 
 // Set the app to listen on port 8080
